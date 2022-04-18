92d88af75c687ac08c30d283a43aec3ad6238c4e
Julio.S <just.julio95@gmail.com>
2022-04-18 14:49:03 -0400

    stored search to history, made buttons functional, added icons where appropriate, uv-index color coded, and began playing with general style and formatting

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,9 @@
 var apiKey = '876a9cd8007aec3aef4fd80583307842';
 // and let's create element var's to play with
 
+//var to play with the hidden display until the user interacts with the app
+var genDisplay = document.getElementById("general-display")
+
 // input from the search bar
 var citySearchEl = $("#city");
 // display for city name on the page
@@ -12,13 +15,6 @@ var currentWeather = $("#current-weather")
 var fiveDayDisplay = $("#five-day-forecast")
 // create an array for the purpose of monitoring all the buttons
 var searchedCities = [];
-
-/* DATA TO FOCUS ON:
-    TEMP
-    WIND
-    HUMIDITY
-    UV-INDEX [current day only]
-*/
 
 function formSubmitHandler(event) {
     event.preventDefault()
@@ -52,20 +48,26 @@ function getGenInfo(city){
 }
 
 function displayGenInfo(data, city){
+    genDisplay.classList.remove("hidden")
     //get the city name and state together
     var cityState = data[0].name + ", " + data[0].state;
     $("#searched-city").text(cityState)
+    
+    createButton(cityState)
+}
 
+//create a function for the dynamic button creation
+function createButton(name) {
     // create a button with the data that's present.
     var recentSearch = document.createElement("button");
-    recentSearch.setAttribute("class", "btn btn-primary")
+    recentSearch.setAttribute("class", "btn blue-gradient text-light mb-2 ml-2")
     recentSearch.setAttribute("id", "history")
-    recentSearch.textContent = data[0].name;
+    recentSearch.textContent = name;
     //Make sure that no duplicate buttons can be made
     if (!searchedCities.includes(recentSearch.textContent)){
-        //debugger;
         searchedCities.push(recentSearch.textContent);
-        $("#recent-searches").append(recentSearch)
+        $("#recent-searches").append(recentSearch);
+        saveCities();
     }
 }
 
@@ -91,62 +93,111 @@ function displayCurrentInfo(weather) {
     //clear out the list for every call
     currentWeather.children().remove()
 
+    //pull the weather icon link
+    var weatherImg = "http://openweathermap.org/img/wn/" + weather.weather[0].icon + ".png"
+    var weatherAlt = weather.weather[0].main + ": " + weather.weather[0].description;
+
     // var for the current Date-
     var currentDate = new Date(weather.dt * 1000).toLocaleDateString()
     //append to the screen
     $("#searched-city").append(" " + currentDate)
+    $("#searched-city").append("<img class='icon' src='" + weatherImg + "' alt='" + weatherAlt + "'></>");
 
     // var for current temp, wind speed, humidity, and UVI
     var currentTemp = weather.temp;
     var currentWndSpd = weather.wind_speed + " MPH";
-    var currentHmdty = weather.humidity + "%";
+    var currentHmdty = weather.humidity + " %"
     var currentUvi = weather.uvi;
 
     //Append info to the screen
-    $("#current-weather").append("<li class='list-item'>Temperature: " + currentTemp + "</li>")
-    $("#current-weather").append("<li class='list-item'>Wind Speed: " + currentWndSpd + "</li>")
-    $("#current-weather").append("<li class='list-item'>Humidity: " + currentHmdty + "</li>")
-    $("#current-weather").append("<li class='list-item'>UV-Index: " + currentUvi + "</li>")
+    $("#current-weather").append("<p class='list-item'>Temperature: <span id='temp'>" + currentTemp + "</span></p>")
+    $("#current-weather").append("<p class='list-item'>Wind Speed: <span id='wind'>" + currentWndSpd + "</span></p>")
+    $("#current-weather").append("<p class='list-item'>Humidity: <span id='humidity'>" + currentHmdty + "</span></p>")
+    $("#current-weather").append("<p class='list-item'>UV-Index: <span id='uvi'>" + currentUvi + "</span></p>")
+    checkUv(currentUvi)
+}
+
+function checkUv(uvi){
+    if(uvi <= 4) {
+        $("#uvi").attr("class", "bg-success")
+    } else if(uvi > 4 && uvi <= 7) {
+        $("#uvi").attr("class", "bg-warning")
+    } else {
+        $("#uvi").attr("class", "bg-danger")
+    }
 }
 
 function displayFiveDayInfo(futureWeather){
     fiveDayDisplay.children().remove()
 
-    //console.log(futureWeather)
+    console.log(futureWeather)
 
     for(var i = 1; i < 6; i++){
         var dailyForecast = futureWeather[i];
+        // acquire the icon and description of the weather for each day
+        var forecastWeatherImg = "http://openweathermap.org/img/wn/" + dailyForecast.weather[0].icon + "@2x.png"
+        var forecastWeatherAlt = dailyForecast.weather[0].main + ": " + dailyForecast.weather[0].description;
 
         //create a div to put the weather cards up
         var forecastCard = document.createElement("div");
-        forecastCard.setAttribute("class", "card")
-        //console.log(forecastCard);
+        forecastCard.setAttribute("class", "card mx-3")
 
         // get the forecast date to the screen
         var forecastDate = document.createElement("h4")
-        forecastDate.setAttribute("class", "card-header")
+        forecastDate.setAttribute("class", "card-header text-center text-light bg-primary")
         forecastDate.textContent = new Date(dailyForecast.dt * 1000).toLocaleDateString();
         forecastCard.appendChild(forecastDate);
 
+        var forecastIcon = document.createElement("img");
+        forecastIcon.setAttribute("src", forecastWeatherImg);
+        forecastIcon.setAttribute("alt", forecastWeatherAlt);
+        forecastIcon.setAttribute("class", "m-auto w-50")
+        forecastCard.appendChild(forecastIcon);
+
         //get forecast temp to the screen
         var forecastTemp = document.createElement("span");
-        forecastTemp.setAttribute("class", "list-item");
+        forecastTemp.setAttribute("class", "list-item mb-2");
         forecastTemp.textContent = "Temperature: " + dailyForecast.temp.day;
         forecastCard.appendChild(forecastTemp);
 
         //get forecast wind to screen
         var forecastWind = document.createElement("span");
-        forecastWind.setAttribute("class", "list-item");
+        forecastWind.setAttribute("class", "list-item mb-2");
         forecastWind.textContent = "Wind Speed: " + dailyForecast.wind_speed + " MPH";
         forecastCard.appendChild(forecastWind);
 
         //get forecast humidity to screen
         var forecastHmdty = document.createElement("span")
-        forecastHmdty.setAttribute("class", "list-item");
+        forecastHmdty.setAttribute("class", "list-item mb-2");
         forecastHmdty.textContent = "Humidity: " + dailyForecast.humidity + "%";
         forecastCard.appendChild(forecastHmdty);
 
+        //get uvi data to screen
+        var forecastUvi = document.createElement("span");
+        forecastUvi.setAttribute("class", "list-item mb-2");
+        forecastUvi.textContent = "UV-Index: " + dailyForecast.uvi;
+        forecastCard.appendChild(forecastUvi);
+
         fiveDayDisplay.append(forecastCard)
+    }
+}
+
+function saveCities() {
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
+}
+
+function loadCities() {
+    var searchHistory = localStorage.getItem("cities");
+
+    if(!searchHistory){
+        return false;
+    } else 
+    {
+        searchHistory = JSON.parse(searchHistory)
+
+        for(var i = 0; i < searchHistory.length; i++) {
+            createButton(searchHistory[i])
+        }
     }
 }
 
@@ -154,6 +205,7 @@ $("#search-btn").on("click", formSubmitHandler)
 
 $("#recent-searches").on("click", "button", function() {
     var priorSearch = $(this).text()
-    console.log(priorSearch);
     getGenInfo(priorSearch)
 })
+
+loadCities();
